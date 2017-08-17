0839076d0c2c7ee9efaf74b3a0f23c3f6c26d672
claudiomelendres <claudio.melendres@incontact.com>
2017-08-16 23:00:29 -0400

    first commit

---
+++ OrdersWeb/Controllers/CatalogsController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OrdersWeb.Controllers
+{
+    public class CatalogsController : Controller
+    {
+        // GET: Category
+        [Authorize]
+        public ActionResult Categories()
+        {
+            return View();
+        }
+        [Authorize]
+        public ActionResult Products()
+        {
+            return View();
+        }
+
+    }
+}
