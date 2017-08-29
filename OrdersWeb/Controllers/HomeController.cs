c237ced15262dbe63ebb1af417cf4ead83d4f421
claudiomelendres <claudio.melendres@incontact.com>
2017-08-28 21:30:16 -0400

    add html

--- OrdersWeb/Controllers/HomeController.cs
+++ OrdersWeb/Controllers/HomeController.cs
@@ -26,5 +26,78 @@ namespace OrdersWeb.Controllers
 
             return View();
         }
+
+        public ActionResult Auto1()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Auto2()
+        {
+            return View();
+        }
+
+        public ActionResult Auto3()
+        {
+            return View();
+        }
+
+        public ActionResult Auto4()
+        {
+            return View();
+        }
+
+        public ActionResult Auto5()
+        {
+            return View();
+        }
+
+        public ActionResult Auto6()
+        {
+            return View();
+        }
+
+        public ActionResult Auto7()
+        {
+            return View();
+        }
+        public ActionResult Auto8()
+        {
+            return View();
+        }
+        public ActionResult Auto9()
+        {
+            return View();
+        }
+        public ActionResult Auto10()
+        {
+            return View();
+        }
+        public ActionResult Auto11()
+        {
+            return View();
+        }
+
+        public ActionResult Auto12()
+        {
+            return View();
+        }
+
+        public ActionResult Auto13()
+        {
+            return View();
+        }
+
+        public ActionResult Auto14()
+        {
+            return View();
+        }
+
+        public ActionResult Auto15()
+        {
+            return View();
+        }
     }
 }
